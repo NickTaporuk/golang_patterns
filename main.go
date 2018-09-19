f284044582bb2937d5af36f691c9f1e6e947c7b7
Mykola Kuropatkin <mykola.kuropatkin@continuum.com>
2018-09-19 11:03:27 +0300

    pattern chain of responsobility done

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"github.com/NickTaporuk/golang_patterns/behavioral/chain_of_resposibility"
+)
+
 func main() {
-	
+
+	handlers := &chain_of_responsibility.ConcreteHandlerA{
+		Next:&chain_of_responsibility.ConcreteHandlerB{
+			Next:&chain_of_responsibility.ConcreteHandlerC{},
+		},
+	}
+
+	fmt.Println(handlers.SendRequest(4))
 }
