d92ae8de159eee6f75de1d5bd3a577c6dd8dfe62
Mykola Kuropatkin <mykola.kuropatkin@continuum.com>
2018-09-19 16:48:57 +0300

    builder test updated

--- behavioral/iterator/iterator.go
+++ behavioral/iterator/iterator.go
@@ -1,20 +1,22 @@
 package iterator
 
-//var sum int = 0
-//it := NewIntStatefulIterator(int_data)
-//for it.Next() {
-//    sum += it.Value()
-//}
+type StatefulIterator interface {
+	Value() int
+	Next() bool
+}
 
+//
 type intStatefulIterator struct {
 	current int
 	data    []int
 }
 
+//
 func (it *intStatefulIterator) Value() int {
 	return it.data[it.current]
 }
 
+//
 func (it *intStatefulIterator) Next() bool {
 	it.current++
 	if it.current >= len(it.data) {
@@ -23,6 +25,15 @@ func (it *intStatefulIterator) Next() bool {
 	return true
 }
 
+//
 func NewIntStatefulIterator(data []int) *intStatefulIterator {
 	return &intStatefulIterator{data: data, current: -1}
 }
+
+// Example :
+//var sum int = 0
+// int_data := []int{1,2,3,4,5,6,7,89}
+//it := NewIntStatefulIterator(int_data)
+//for it.Next() {
+//    sum += it.Value()
+//}
