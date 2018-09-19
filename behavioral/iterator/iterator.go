cb2c1cf73be2e380dce9223c71ea51f3765f07b1
Mykola Kuropatkin <mykola.kuropatkin@continuum.com>
2018-09-19 14:32:00 +0300

    pattern iterator done

--- behavioral/iterator/iterator.go
+++ behavioral/iterator/iterator.go
@@ -1,1 +1,28 @@
 package iterator
+
+//var sum int = 0
+//it := NewIntStatefulIterator(int_data)
+//for it.Next() {
+//    sum += it.Value()
+//}
+
+type intStatefulIterator struct {
+	current int
+	data    []int
+}
+
+func (it *intStatefulIterator) Value() int {
+	return it.data[it.current]
+}
+
+func (it *intStatefulIterator) Next() bool {
+	it.current++
+	if it.current >= len(it.data) {
+		return false
+	}
+	return true
+}
+
+func NewIntStatefulIterator(data []int) *intStatefulIterator {
+	return &intStatefulIterator{data: data, current: -1}
+}
